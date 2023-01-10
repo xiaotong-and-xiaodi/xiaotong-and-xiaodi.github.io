6fa1be6302d006e73fc30eb03e1a0846d2b9ce1a
xiaotong-and-xiaodi <wudi0971@163.com>
2023-01-10 13:32:32 +0800

    Site updated: 2023-01-10 13:32:32

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("小童同学！");
+        ("小童宝宝！");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
